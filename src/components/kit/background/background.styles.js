9f4bd3defc160e3c4743b02c370a2cde2f85f00f
Alex Busch <abusch1013@gmail.com>
2019-08-30 10:53:26 -0700

    run prettier

--- src/components/kit/background/background.styles.js
+++ src/components/kit/background/background.styles.js
@@ -22,8 +22,8 @@ const StyledBackground = styled.div`
 `;
 
 const StyledBackgroundDevices = styled(animated.div)`
-    display: flex;
-    align-self: flex-start;
+  display: flex;
+  align-self: flex-start;
 `;
 
 const StyledBackgroundTablet = styled.div`
@@ -37,7 +37,9 @@ const StyledBackgroundTablet = styled.div`
   z-index: 1;
   border: 1px solid ${props => props.theme.colors.grey};
   background-color: ${props => props.theme.colors.white};
-  box-shadow: inset 0 4px 7px 1px #fefefe, inset 0 -5px 20px rgba(173,186,204,.25), 0 2px 6px rgba(0,21,64,.14), 0 10px 20px rgba(0,21,64,.05);
+  box-shadow: inset 0 4px 7px 1px #fefefe,
+    inset 0 -5px 20px rgba(173, 186, 204, 0.25), 0 2px 6px rgba(0, 21, 64, 0.14),
+    0 10px 20px rgba(0, 21, 64, 0.05);
   transform: translateY(${props => props.offset}px);
 `;
 
@@ -52,45 +54,51 @@ const StyledBackgroundPhone = styled.div`
   z-index: 1;
   border: 1px solid ${props => props.theme.colors.grey};
   background-color: ${props => props.theme.colors.white};
-  box-shadow: inset 0 4px 7px 1px #fefefe, inset 0 -5px 20px rgba(173,186,204,.25), 0 2px 6px rgba(0,21,64,.14), 0 10px 20px rgba(0,21,64,.05);
+  box-shadow: inset 0 4px 7px 1px #fefefe,
+    inset 0 -5px 20px rgba(173, 186, 204, 0.25), 0 2px 6px rgba(0, 21, 64, 0.14),
+    0 10px 20px rgba(0, 21, 64, 0.05);
   transform: translateY(${props => props.offset}px);
 `;
 
 const StyledBackgroundBigPhone = styled.div`
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    width: 267px;
-    height: 553px;
-    margin-left: 50px;
-    border-radius: 38px;
-    border: 1px solid ${props => props.theme.colors.grey};
-    background-color: ${props => props.theme.colors.white};
-    z-index: 1;
-    box-shadow: inset 0 4px 7px 1px #fefefe, inset 0 -5px 20px rgba(173,186,204,.25), 0 2px 6px rgba(0,21,64,.14), 0 10px 20px rgba(0,21,64,.05);
-    transform: translateY(${props => props.offset}px);
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 267px;
+  height: 553px;
+  margin-left: 50px;
+  border-radius: 38px;
+  border: 1px solid ${props => props.theme.colors.grey};
+  background-color: ${props => props.theme.colors.white};
+  z-index: 1;
+  box-shadow: inset 0 4px 7px 1px #fefefe,
+    inset 0 -5px 20px rgba(173, 186, 204, 0.25), 0 2px 6px rgba(0, 21, 64, 0.14),
+    0 10px 20px rgba(0, 21, 64, 0.05);
+  transform: translateY(${props => props.offset}px);
 `;
 
 const StyledBackgroundBigTablet = styled.div`
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    width: 450px;
-    height: 675px;
-    margin-left: 50px;
-    border-radius: 38px;
-    background-color: ${props => props.theme.colors.white};
-    border: 1px solid ${props => props.theme.colors.grey};
-    z-index: 1;
-    box-shadow: inset 0 4px 7px 1px #fff, inset 0 -5px 20px rgba(173,186,204,.25), 0 2px 6px rgba(0,21,64,.14), 0 10px 20px rgba(0,21,64,.05);
-    transform: translateY(${props => props.offset}px);
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 450px;
+  height: 675px;
+  margin-left: 50px;
+  border-radius: 38px;
+  background-color: ${props => props.theme.colors.white};
+  border: 1px solid ${props => props.theme.colors.grey};
+  z-index: 1;
+  box-shadow: inset 0 4px 7px 1px #fff,
+    inset 0 -5px 20px rgba(173, 186, 204, 0.25), 0 2px 6px rgba(0, 21, 64, 0.14),
+    0 10px 20px rgba(0, 21, 64, 0.05);
+  transform: translateY(${props => props.offset}px);
 `;
 
 export {
-    StyledBackground,
-    StyledBackgroundDevices,
-    StyledBackgroundTablet,
-    StyledBackgroundPhone,
-    StyledBackgroundBigPhone,
-    StyledBackgroundBigTablet,
+  StyledBackground,
+  StyledBackgroundDevices,
+  StyledBackgroundTablet,
+  StyledBackgroundPhone,
+  StyledBackgroundBigPhone,
+  StyledBackgroundBigTablet,
 };
--- src/components/kit/background/index.js
+++ src/components/kit/background/index.js
@@ -1,13 +1,13 @@
 import { useState, useEffect } from 'react';
-import { useSpring } from "react-spring";
-import { useMeasure } from "../../../hooks";
+import { useSpring } from 'react-spring';
+import { useMeasure } from '../../../hooks';
 import {
-    StyledBackground,
-    StyledBackgroundDevices,
-    StyledBackgroundTablet,
-    StyledBackgroundPhone,
-    StyledBackgroundBigPhone,
-    StyledBackgroundBigTablet,
+  StyledBackground,
+  StyledBackgroundDevices,
+  StyledBackgroundTablet,
+  StyledBackgroundPhone,
+  StyledBackgroundBigPhone,
+  StyledBackgroundBigTablet,
 } from './background.styles';
 
 function Background(props) {
@@ -15,40 +15,40 @@ function Background(props) {
   const [scrollY, setScrollY] = useState(0);
 
   function handleWindowScroll() {
-      setScrollY(window.scrollY);
-  };
+    setScrollY(window.scrollY);
+  }
 
   useEffect(() => {
-      window.addEventListener('scroll', handleWindowScroll);
-      return () => window.removeEventListener('scroll', handleWindowScroll);
+    window.addEventListener('scroll', handleWindowScroll);
+    return () => window.removeEventListener('scroll', handleWindowScroll);
   }, [window.scrollY]);
 
   const spring = useSpring({
-      transform: `translate3d(-${scrollY}px, 80px, 0px)`,
+    transform: `translate3d(-${scrollY}px, 80px, 0px)`,
   });
 
   return (
     <StyledBackground {...bind} userMenuOpen={props.userMenuOpen}>
-        <StyledBackgroundDevices style={spring}>
-            <StyledBackgroundTablet offset={100}>
-                <img src="/static/code-tablet.png" />
-            </StyledBackgroundTablet>
-            <StyledBackgroundBigPhone offset={0}>
-                <img src="/static/phone-big-wireframe.png" />
-            </StyledBackgroundBigPhone>
-            <StyledBackgroundBigTablet offset={100}>
-                <img src="/static/tablet-big-wireframe.png" />
-            </StyledBackgroundBigTablet>
-            <StyledBackgroundPhone offset={0}>
-                <img src="https://stripe.com/img/v3/home/app-illustrations/postmates.svg" />
-            </StyledBackgroundPhone>
-            <StyledBackgroundTablet offset={0}>
-                <img src="https://stripe.com/img/v3/home/app-illustrations/salesforce.svg" />
-            </StyledBackgroundTablet>
-            <StyledBackgroundTablet>
-                <img src="https://stripe.com/img/v3/home/app-illustrations/salesforce.svg" />
-            </StyledBackgroundTablet>
-        </StyledBackgroundDevices>
+      <StyledBackgroundDevices style={spring}>
+        <StyledBackgroundTablet offset={100}>
+          <img src="/static/code-tablet.png" />
+        </StyledBackgroundTablet>
+        <StyledBackgroundBigPhone offset={0}>
+          <img src="/static/phone-big-wireframe.png" />
+        </StyledBackgroundBigPhone>
+        <StyledBackgroundBigTablet offset={100}>
+          <img src="/static/tablet-big-wireframe.png" />
+        </StyledBackgroundBigTablet>
+        <StyledBackgroundPhone offset={0}>
+          <img src="https://stripe.com/img/v3/home/app-illustrations/postmates.svg" />
+        </StyledBackgroundPhone>
+        <StyledBackgroundTablet offset={0}>
+          <img src="https://stripe.com/img/v3/home/app-illustrations/salesforce.svg" />
+        </StyledBackgroundTablet>
+        <StyledBackgroundTablet>
+          <img src="https://stripe.com/img/v3/home/app-illustrations/salesforce.svg" />
+        </StyledBackgroundTablet>
+      </StyledBackgroundDevices>
     </StyledBackground>
   );
 }
