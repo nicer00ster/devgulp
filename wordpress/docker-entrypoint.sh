c9d7d1281eb66378bfa3ee451cd355ad64868d9d
cw0102 <18674410+cw0102@users.noreply.github.com>
2019-12-17 17:33:06 -0500

    -Pin wordpress to 5.2.x -Remove rsync dev dependency

--- wordpress/docker-entrypoint.sh
+++ wordpress/docker-entrypoint.sh
@@ -179,9 +179,12 @@ EOPHP
 	fi
 
 	# Copy over the application files to ensure they are the latest version
-	rsync -rI --exclude=/wp-includes/functions.php /opt/app/. /var/www/html/
-	if [[ -w /var/www/html/wp-includes/functions.php ]]; then
-		cp -f /opt/app/wp-includes/functions.php /var/www/html/wp-includes/functions.php
+	if [[ -L /var/www/html/wp-includes/functions.php ]]; then
+		rm /var/www/html/wp-includes/functions.php
+	fi
+	cp -rf /opt/app/. /var/www/html/
+	if [[ -e /opt/mount/functions.php ]]; then
+		ln -sf /opt/mount/functions.php /var/www/html/wp-includes/functions.php
 	fi
 
 	# If the instance has not yet been configured, copy the plugins and API and run the wordpress install
