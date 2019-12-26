7ff3e8865222eef7b9502093f91a68828277a8ee
Alex Busch <abusch1013@gmail.com>
2019-12-26 11:28:16 -0500

    prettier formatting

--- frontend/src/components/kit/emotes/index.js
+++ frontend/src/components/kit/emotes/index.js
@@ -1,38 +1,46 @@
 import { Picker } from 'emoji-mart';
 
 function Emotes(props) {
-    const emojiData = props.emotes.map(emoji => {
-        return {
-            name: emoji.split('.').slice(0, -1).join('.'),
-            short_names: [emoji.split('.').slice(0, -1).join('.')],
-            text: '',
-            emoticons: [],
-            keywords: ['blob'],
-            imageUrl: `/static/emotes/blobs/${emoji}`,
-        }
-    });
+  const emojiData = props.emotes.map(emoji => {
+    return {
+      name: emoji
+        .split('.')
+        .slice(0, -1)
+        .join('.'),
+      short_names: [
+        emoji
+          .split('.')
+          .slice(0, -1)
+          .join('.'),
+      ],
+      text: '',
+      emoticons: [],
+      keywords: ['blob'],
+      imageUrl: `/static/emotes/blobs/${emoji}`,
+    };
+  });
 
-    return (
-        <Picker
-            onClick={emoji => props.addEmoji(emoji)}
-            color="#80dad3"
-            i18n={{
-                categories: {
-                    custom: 'Blobs',
-                }
-            }}
-            include={['recent', 'custom', 'people', 'foods']}
-            showPreview={false}
-            showSkinTones={false}
-            set="twitter"
-            custom={emojiData}
-            icons={{
-                categories: {
-                    custom: () => <img src='/static/emotes/blobs/blob-outline.png' />
-                }
-            }}
-        />
-    );
+  return (
+    <Picker
+      onClick={emoji => props.addEmoji(emoji)}
+      color="#80dad3"
+      i18n={{
+        categories: {
+          custom: 'Blobs',
+        },
+      }}
+      include={['recent', 'custom', 'people', 'foods']}
+      showPreview={false}
+      showSkinTones={false}
+      set="twitter"
+      custom={emojiData}
+      icons={{
+        categories: {
+          custom: () => <img src="/static/emotes/blobs/blob-outline.png" />,
+        },
+      }}
+    />
+  );
 }
 
 export default Emotes;
