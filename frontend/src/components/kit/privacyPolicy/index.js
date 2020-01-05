b4055cc9e9debb248ef23fcfa8fa9e09a204f2b7
Alex Busch <abusch1013@gmail.com>
2020-01-04 18:06:34 -0700

    update privacy policy frontend

--- frontend/src/components/kit/privacyPolicy/index.js
+++ frontend/src/components/kit/privacyPolicy/index.js
@@ -1,8 +1,14 @@
-import { StyledPrivacyPolicy } from './privacyPolicy.styles';
+import {
+  StyledPrivacyPolicy,
+  StyledPrivacyPolicyHeading,
+} from './privacyPolicy.styles';
 
 function PrivacyPolicy(props) {
   return (
     <StyledPrivacyPolicy>
+      <StyledPrivacyPolicyHeading>
+        {props.page.title.rendered}
+      </StyledPrivacyPolicyHeading>
       <div dangerouslySetInnerHTML={{ __html: props.page.content.rendered }} />
     </StyledPrivacyPolicy>
   );
--- frontend/src/components/kit/privacyPolicy/privacyPolicy.styles.js
+++ frontend/src/components/kit/privacyPolicy/privacyPolicy.styles.js
@@ -1,5 +1,15 @@
 import styled from 'styled-components';
 
-const StyledPrivacyPolicy = styled.div``;
+const StyledPrivacyPolicy = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: flex-start;
+  background-color: ${props => props.theme.colors.white};
+  border-radius: ${props => props.theme.effects.radius};
+  box-shadow: ${props => props.theme.effects.shadow};
+  padding: 1.2rem;
+`;
 
-export { StyledPrivacyPolicy };
+const StyledPrivacyPolicyHeading = styled.h1``;
+
+export { StyledPrivacyPolicy, StyledPrivacyPolicyHeading };
