c56ead70306258c0c5f6db2fd063aa3a857494f1
Alex Busch <abusch1013@gmail.com>
2020-02-28 12:52:57 -0700

    update the listener for login menu so you can close menu with ESC key

--- frontend/src/components/kit/userMenu/index.js
+++ frontend/src/components/kit/userMenu/index.js
@@ -41,13 +41,14 @@ function UserMenu(props) {
 
   return trail.map(({ x, height, opacity, ...rest }, index) => (
     <StyledUserMenu
+      ref={props.userMenuRef}
       key={index}
+      disabled={!props.userMenuOpen || props.user.isLoggingOut}
       style={{
         transform: x.interpolate(x => `translate3d(0,${x}px,0)`),
         opacity: !props.user.isLoggingOut && opacity,
         ...rest,
-      }}
-      disabled={!props.userMenuOpen || props.user.isLoggingOut}>
+      }}>
       {props.userMenuOpen && (
         <StyledUserMenuList>
           <StyledUserDataListItem>
